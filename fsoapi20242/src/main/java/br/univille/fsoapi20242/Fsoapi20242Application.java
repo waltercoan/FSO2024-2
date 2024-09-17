f25fa46f790fa30a4df640ee70cfd7af8d891cff
WALTER SILVESTRE COAN <walter.s@univille.br>
2024-09-17 18:58:39 -0300

    projeto API

---
+++ fsoapi20242/src/main/java/br/univille/fsoapi20242/Fsoapi20242Application.java
@@ -0,0 +1,13 @@
+package br.univille.fsoapi20242;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fsoapi20242Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fsoapi20242Application.class, args);
+	}
+
+}
---
+++ fsoapi20242/src/test/java/br/univille/fsoapi20242/Fsoapi20242ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.fsoapi20242;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fsoapi20242ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
